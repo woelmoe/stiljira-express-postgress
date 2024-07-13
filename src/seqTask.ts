081faa28aa7a287e3be51b07acfb004cb9e2bbef
holodnyak <holodnyak@stilsoft.ru>
2024-07-13 22:30:43 +0300

    Выполнил модель таски в бд

--- src/seqTask.ts
+++ src/seqTask.ts
@@ -1,13 +1,26 @@
-import { Sequelize } from 'sequelize'
+import { INTEGER, STRING } from 'sequelize'
+
+export interface ITask {
+  id: number
+  description: string
+  content: string
+  type: TaskType
+  title?: string
+  editable?: boolean
+}
 
 import { seqDatabase } from './seqDatabase'
-// const User = seqDatabase.define('user', {
-//   id: {
-//     type: Sequelize.INTEGER,
-//     autoIncrement: true,
-//     allowNull: false,
-//     primaryKey: true
-//   },
-//   name: Sequelize.STRING,
-//   email: Sequelize.STRING
-// })
+import { TaskType } from './interfaces/interfaces'
+const Task = seqDatabase.define('task', {
+  id: {
+    type: INTEGER,
+    autoIncrement: true,
+    allowNull: false,
+    primaryKey: true
+  },
+  counter: INTEGER,
+  title: STRING,
+  type: STRING,
+  content: STRING,
+  description: STRING
+})
