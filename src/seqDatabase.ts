449faee1a6f3f57daf285ddbc15574a258f7511a
holodnyak <holodnyak@stilsoft.ru>
2024-08-02 20:07:46 +0300

    Откат к рабочей версии

--- src/seqDatabase.ts
+++ src/seqDatabase.ts
@@ -1,12 +1,13 @@
 import { Sequelize } from 'sequelize'
 import 'dotenv/config'
 
+const isDev = process.env.IS_DEV === 'true'
 export const seqDatabase = new Sequelize(
   process.env.PG_DB,
   process.env.PG_USER,
   process.env.PG_PASSWORD,
   {
-    host: process.env.IS_DEV ? 'localhost' : process.env.PG_HOST,
+    host: isDev ? 'localhost' : process.env.PG_HOST,
     dialect: 'postgres'
   }
 )
